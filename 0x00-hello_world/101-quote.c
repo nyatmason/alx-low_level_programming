2d09db4883e91252d31d23c2ff0f04323246f76c
nyatmason <nyatefe.mawuenyefia@gmail.com>
2023-02-17 08:00:18 -0800

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 /**
- * main - Entry point
- * Return: Always 1
+ * main - A c program that prints a line to the standard error
+ * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
