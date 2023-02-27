a8808440fa057a928ee16a033c1a5e88fffd39cb
nyatmason <nyatefe.mawuenyefia@gmail.com>
2023-02-27 12:52:59 -0800

    f

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: declaration of n and paramters for the function reset_to_98
+ * Return: Always 0.
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
